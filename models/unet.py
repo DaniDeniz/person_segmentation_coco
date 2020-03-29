0628483fbb95b13fbb3eb140726a4decc365d9ab
DaniDeniz <dani.deniz@hotmail.com>
2020-03-29 11:24:18 +0100

    minor fix

--- models/unet.py
+++ models/unet.py
@@ -99,7 +99,7 @@ def Unet(nClasses, input_height=256, input_width=256, nChannels=3):
 
 
     if nClasses == 2:
-        conv11 = Conv2D(1, (1, 1), padding='same', activation='relu',
+        conv11 = Conv2D(1, (1, 1), padding='same',
                         kernel_initializer=he_normal(), kernel_regularizer=l2(0.005))(conv10)
 
         conv11= Activation("sigmoid")(conv11)
