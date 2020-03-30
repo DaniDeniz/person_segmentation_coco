ff947ce639a15786441fab9a3f397f8b60ff30fb
DaniDeniz <dani.deniz@hotmail.com>
2020-03-30 13:10:50 +0100

    Convert to RGB

--- utils/segdata_generator.py
+++ utils/segdata_generator.py
@@ -77,6 +77,7 @@ def preprocess(image, height=224, width=224):
         img = cv2.resize(image, (width, new_height))
         im[diff:diff + new_height, :, :] = img
 
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     im_norm = scale_frame(im)
     return im_norm, im
 
@@ -123,7 +124,7 @@ def preprocess_with_label(image, label, height, width, n_classes):
     for c in range(n_classes):
         seg_labels[:, :, c] = (lim == c).astype(int)
     seg_labels = np.reshape(seg_labels, (width * height, n_classes))
-
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     im = scale_frame(im)
     return im, seg_labels
 
