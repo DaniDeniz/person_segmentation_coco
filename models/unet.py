50b16580f147207a83a58e9f448ce0ee3cd92d6e
DaniDeniz <dani.deniz@hotmail.com>
2020-05-19 17:05:20 +0100

    Added mini reduced

--- models/unet.py
+++ models/unet.py
@@ -68,6 +68,55 @@ def Unet(nClasses, input_height=256, input_width=256, nChannels=3):
 
     return model
 
+def Unet_mini_reduced(nClasses, input_height=256, input_width=256, nChannels=3):
+    inputs = Input(shape=(input_height, input_width, nChannels))
+    # encode
+    # 224x224
+    conv1 = Conv2D(64, (3, 3), padding='same', activation="relu", kernel_initializer=orthogonal())(inputs)
+    conv1 = SpatialDropout2D(0.2)(conv1)
+    conv1 = Conv2D(64, (3, 3), padding='same', activation="relu", kernel_initializer=orthogonal())(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    # 112x112
+    conv2 = Conv2D(128, (3, 3), padding='same', activation="relu", kernel_initializer=orthogonal())(pool1)
+    conv2 = SpatialDropout2D(0.2)(conv2)
+    conv2 = Conv2D(128, (3, 3), padding='same', activation="relu",kernel_initializer=orthogonal())(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # 56x56
+    conv3 = Conv2D(256, (3, 3), padding='same', activation="relu",kernel_initializer=orthogonal())(pool2)
+    conv3 = SpatialDropout2D(0.2)(conv3)
+    conv3 = Conv2D(256, (3, 3), padding='same', activation="relu",kernel_initializer=orthogonal())(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # 28x28
+    conv4 = Conv2D(256, (3, 3), padding='same', activation="relu", kernel_initializer=orthogonal())(pool3)
+    conv4 = SpatialDropout2D(0.2)(conv4)
+    conv4 = Conv2D(256, (3, 3), padding='same', activation="relu", kernel_initializer=orthogonal())(conv4)
+
+    up7 = UpSampling2D(size=(2, 2))(conv4)
+    up7 = concatenate([up7, conv3], axis=-1)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=orthogonal())(up7)
+    conv7 = SpatialDropout2D(0.2)(conv7)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=orthogonal())(conv7)
+
+    up8 = UpSampling2D(size=(2, 2))(conv7)
+    up8 = concatenate([up8, conv2], axis=-1)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=orthogonal())(up8)
+    conv8 = SpatialDropout2D(0.2)(conv8)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=orthogonal())(conv8)
+
+    up9 = UpSampling2D(size=(2, 2))(conv8)
+    up9 = concatenate([up9, conv1], axis=-1)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=orthogonal())(up9)
+    conv9 = SpatialDropout2D(0.2)(conv9)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=orthogonal())(conv9)
+
+    final_activation = "sigmoid"
+    if nClasses > 1:
+        final_activation = "softmax"
+    conv10 = Conv2D(nClasses, (1, 1), activation=final_activation)(conv9)
+
+    model = Model(input=inputs, output=conv10)
+
+    return model
 
 def Unet_mini(nClasses, input_height=256, input_width=256, nChannels=3):
     inputs = Input(shape=(input_height, input_width, nChannels))
